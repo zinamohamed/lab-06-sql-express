e85457226e04019a63990d2bfc0934c4cc9f6730
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-02-19 23:04:31 -0800

    first test passing

---
+++ __tests__/endpoints.tests.js
@@ -0,0 +1,39 @@
+const { app } = require('../app.js');
+const supertest = require('supertest');
+const request = supertest(app);
+
+it('it should respond with products', async done => {
+    const myData = [
+        {
+            id: 1, 
+            image: 'https://media2.giphy.com/media/aMO3eGc9frP9i24AqG/source.gif',
+            name: 'Niacinamide 10%', 
+            size: '16ml',
+            price: '$15',
+            type: 'Serum'
+        }, 
+        {
+            id: 2, 
+            image: 'https://media2.giphy.com/media/aMO3eGc9frP9i24AqG/source.gif',
+            name: 'Retinol 0.5%', 
+            size: '12ml',
+            price: '$35',
+            type: 'Exfoliant'
+        },
+        {
+            id: 3, 
+            image: 'https://media2.giphy.com/media/aMO3eGc9frP9i24AqG/source.gif',
+            name: 'AHA+BHA Solution', 
+            size: '12ml',
+            price: '$50',
+            type: 'Exfoliant'
+        },
+    ];
+    const response = await request.get('/products');
+
+    expect(response.status).toBe(200);
+    
+    expect(response.body).toEqual({ results: myData });
+    
+    done();
+});
